f9530f2dd11b1e3dc5b86093182abdff3b56df5a
boletimiasd7 <262976010+boletimiasd7@users.noreply.github.com>
2026-02-22 07:03:08 -0300

    Primeiro commit do backend (#2)

---
+++ src/back/boletim/boletim/Controllers/IgrejaController.cs
@@ -0,0 +1,19 @@
+﻿using boletim.Models.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace boletim.Controllers
+{
+    [Route("api/igreja")]
+    [ApiController]
+    public class IgrejaController : ControllerBase
+    {
+        [HttpGet("listar")]
+        public IActionResult Listar()
+        {
+            return Ok(new List<IgrejaListagem> {
+            new IgrejaListagem { Id = 1, Nome = "604 Norte" },
+            new IgrejaListagem { Id = 2, Nome = "605 Norte" },
+            });
+        }
+    }
+}
