2bcd89ef4fbba0c916227647eeb11e77d4d8a331
boletimiasd7 <262976010+boletimiasd7@users.noreply.github.com>
2026-02-22 18:32:36 -0300

    Integracao entre back e front (#4)
    * Tela inicial do front-end
    
    * Ajuste na tela de inicialização
    
    * Alteração de Posts para Igrejas
    
    * Adiciona a opão de CORS para qualquer origin, método e cabeçalho
    
    ---------
    
    Co-authored-by: Fabio Lima da Silva <fabbiolimmadasilva@gmail.com>

---
+++ src/front/boletim/src/components/IgrejaItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="post">
+    <p>{{ item.id }}</p>
+    <div>{{ item.nome }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type Igreja from '@/types/Igreja'
+
+interface Props {
+  item: Igreja
+}
+
+const { item } = defineProps<Props>()
+</script>
+
+<style scoped>
+.post {
+  border-bottom: 1px solid #f5f5f5;
+  margin-bottom: 1rem;
+}
+
+.post-details {
+  display: flex;
+}
+</style>
