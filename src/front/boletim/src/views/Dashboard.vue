2f5e5dd18f9b05a85eb7e19e8bcd4c34defe2d1f
Fabio Lima da Silva <262976010+boletimiasd7@users.noreply.github.com>
2026-02-22 18:30:16 -0300

    Alteração de Posts para Igrejas

--- src/front/boletim/src/views/Dashboard.vue
+++ src/front/boletim/src/views/Dashboard.vue
@@ -3,31 +3,30 @@
     <h2>Lista de Posts</h2>
     <p v-if="carregando">Carregando dados da API</p>
     <template v-else>
-      <div v-for="item in posts" :key="item.id">
-        <PostItem :item="item" />
+      <div v-for="item in igrejas" :key="item.id">
+        <IgrejaItem :item="item" />
       </div>
     </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { api } from '@/services/api';
-import type Post from '@/types/Post';
-import PostItem from '@/components/PostItem.vue';
+import { ref, onMounted } from 'vue'
+import { api } from '@/services/api'
+import type Igreja from '@/types/Igreja'
+import IgrejaItem from '@/components/IgrejaItem.vue'
 
-
-const posts = ref<Post[]>([]);
-const carregando = ref(true);
+const igrejas = ref<Igreja[]>([])
+const carregando = ref(true)
 
 onMounted(async () => {
   try {
     // A requisição já aponta para a Base URL e passa o Header correto
-    posts.value = await api.get<Post[]>('/posts');
+    igrejas.value = await api.get<Igreja[]>('/igreja/listar')
   } catch (erro) {
-    alert('Erro ao carregar os dados. Verifique o console.');
+    alert('Erro ao carregar os dados. Verifique o console.')
   } finally {
-    carregando.value = false;
+    carregando.value = false
   }
-});
+})
 </script>
