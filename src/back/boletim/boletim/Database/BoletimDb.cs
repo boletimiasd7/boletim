8b5fb396376d3a8a9d7bf39ff030ef355e2bf30a
Fabio Lima da Silva <262976010+boletimiasd7@users.noreply.github.com>
2026-03-11 22:11:48 -0300

    Configurando banco de dados

---
+++ src/back/boletim/boletim/Database/BoletimDb.cs
@@ -0,0 +1,16 @@
+﻿using boletim.Database.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace boletim.Database
+{
+    public class BoletimDb : DbContext
+    {
+        public BoletimDb(DbContextOptions<BoletimDb> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Igreja> Igreja { get; set; }
+        public DbSet<IgrejaConfig> IgrejaConfig { get; set; }
+    }
+}
---
+++ src/back/boletim/boletim/Database/Entidades/Igreja.cs
@@ -0,0 +1,8 @@
+﻿namespace boletim.Database.Entidades
+{
+    public class Igreja
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
