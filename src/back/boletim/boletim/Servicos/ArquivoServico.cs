921181afc7327ee9c24108fb14cf6726b026486e
boletimiasd7 <262976010+boletimiasd7@users.noreply.github.com>
2026-03-13 13:08:55 -0300

    Upload e download de arquivos no bucket (#12)

---
+++ src/back/boletim/boletim/Servicos/ArquivoServico.cs
@@ -0,0 +1,34 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+
+namespace boletim.Servicos
+{
+    public class ArquivoServico
+    {
+        private readonly StorageClient _storageClient;
+        private const string BUCKET_NAME = "boletim_iasd";
+
+        public ArquivoServico(StorageClient storageClient)
+        {
+            _storageClient = storageClient;
+        }
+
+        public async Task<string> Enviar(string nome, string contentType, MemoryStream stream) {
+
+            string extensao = Path.GetExtension(nome);
+
+            string uuid = $"temp/{Guid.NewGuid()}{extensao}";
+
+            await _storageClient.UploadObjectAsync(BUCKET_NAME, uuid, contentType, stream);
+
+            return uuid;
+        }
+
+        internal async Task<byte[]> Download(string arquivo)
+        {
+            using MemoryStream memoryStream = new MemoryStream();
+            await _storageClient.DownloadObjectAsync(BUCKET_NAME, arquivo, memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+}
